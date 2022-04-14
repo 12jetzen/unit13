d1d8723f7a130aa182c5543a8ed30637203e4773
Fiallos <25nfiallos@FVFY36M9J1WK.tld>
2022-04-14 12:03:50 -0500

    added code

--- app.js
+++ app.js
@@ -6,36 +6,3 @@ function getCard() {
     var r = Math.floor(Math.random() * 10) +1
     document.getElementById("card").innerHTML = "<image src=images/" + r + "C.jpg>"
 }
-function getCard() {
-    var r = Math.floor(Math.random() * 10) +1
-    document.getElementById("card").innerHTML = "<image src=images/" + r + "D.jpg>"
-}
-function getCard() {
-    var r = Math.floor(Math.random() * 10) +1
-    document.getElementById("card").innerHTML = "<image src=images/" + r + "H.jpg>"
-
-}
-function getCard() {
-    var r = Math.floor(Math.random() * 10) +1
-    document.getElementById("card").innerHTML = "<image src=images/" + r + "S.jpg>"
-}
-function getCard() {
-    var r = Math.floor(Math.random() * 10) +1
-    document.getElementById("card").innerHTML = "<image src=images/" + r + "Q.jpg>"
-}
-function getCard() {
-    var r = Math.floor(Math.random() * 10) +1
-    document.getElementById("card").innerHTML = "<image src=images/" + r + "QD.jpg>"
-}
-function getCard() {
-    var r = Math.floor(Math.random() * 10) +1
-    document.getElementById("card").innerHTML = "<image src=images/" + r + "KS.jpg>"
-}
-function getCard() {
-    var r = Math.floor(Math.random() * 10) +1
-    document.getElementById("card").innerHTML = "<image src=images/" + r + "JC.jpg>"
-}
-function getCard() {
-    var r = Math.floor(Math.random() * 10) +1
-    document.getElementById("card").innerHTML = "<image src=images/" + r + "QS.jpg>"
-}
