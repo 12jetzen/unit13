93894c734b06359ef37fe931b5dcd1cae559d7cf
Vazquez Burgos <24jvazquezburgos@FVFZMYH2J1WK.tld>
2022-04-14 11:55:16 -0500

    added new card

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ function getCard() {
     var r = Math.floor(Math.random() * 10) +1
     var suitArray = ["C", "D", "H", "S"]
     var randomSuit = Math.floor(Math.random() * 4) // 0 to 3
-    document.getElementById("card").innerHTML = "<image src=images/" + r + suitArray[randomSuit] + ".jpg>"
+    document.getElementById("card").innerHTML = "<image src=images/" + r + suitArray[randomSuit] + ".jpg"
 }
