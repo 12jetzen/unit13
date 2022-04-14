51ba6e2ff330a2a8fc7940b85df65c8fb39cb1dc
Vazquez Burgos <24jvazquezburgos@FVFZMYH2J1WK.tld>
2022-04-14 11:38:58 -0500

    added person

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ function run() {
 
 function getCard() {
     var r = Math.floor(Math.random() * 10) +1
-    document.getElementById("card").innerHTML = "<image src=images/AC.jpg>"
+    document.getElementById("card").innerHTML = "<image src=images/" + r + "C.jpg>"
+}
+
+function getCard() {
+    var r = Math.floor(Math.random() * 10) +1
+    document.getElementById("card").innerHTML = "<image src=images/" + r + "D.jpg>"
 }
