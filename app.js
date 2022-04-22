37922f9522f56b3a8d7c551988548c2552887a75
Fiallos <25nfiallos@FVFY36M9J1WK.tld>
2022-04-22 11:12:24 -0500

    just updating

--- app.js
+++ app.js
@@ -9,4 +9,6 @@ function getCard() {
     var randomSuit = Math.floor(Math.random() * 4) // 0 to 3
     document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=images/" + r + suitArray[randomSuit] + ".jpg>")
 }
-   
+function getCard() {
+    var r = Math.floor
+}
